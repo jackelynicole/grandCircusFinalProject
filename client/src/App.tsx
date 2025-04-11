4066146d2248db823781f56e049df13c9d8a2183
Jarvis Murray <jarvis011877@gmail.com>
2025-04-10 20:27:52 -0400

    Map view Render

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,32 @@
+import { useRef, useEffect } from 'react'
+import mapboxgl from 'mapbox-gl'
+
+import 'mapbox-gl/dist/mapbox-gl.css';
+
 import './App.css'
 
 function App() {
 
+  const mapRef = useRef()
+  const mapContainerRef = useRef()
+
+  useEffect(() => {
+    mapboxgl.accessToken = 'pk.eyJ1IjoibGlnaHRuaW5nc3ByZWUiLCJhIjoiY205YzB0NnhlMG52ejJrcTJseWxhcXZhZiJ9.VFBHYF5wcCBw35HASKi_eA'
+    mapRef.current = new mapboxgl.Map({
+      container: mapContainerRef.current,
+      center: [-83.0502, 42.3337],
+      zoom: 12.42
+    });
+
+    return () => {
+      mapRef.current.remove()
+    }
+  }, [])
+
   return (
     <>
-      <h1>hello, world!</h1>
+      <h1>hello, world</h1>
+      <div id='map-container' ref={mapContainerRef}/>
     </>
   )
 }
