cab2930788c067b70c6087f48abd52493e4ed4e8
DeVonte Gooden <dg208804@gmail.com>
2025-04-16 19:48:19 -0400

    added code to create and join rooms

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,14 @@
 import Events from "./components/Events"
+import RoomForm from "./components/RoomForm"
 
 function App() {
+  const handleRoomSuccess = (roomCode: string, nickname: string) => {
+    console.log('Joined room:' , roomCode, 'as', nickname)
+  }
   return (
     <>
       <h1 style={{ marginLeft: "10px" }}>What's The Move</h1>
-      <Events />
+      <RoomForm onSuccess={handleRoomSuccess}/>
     </>
   )
 }
